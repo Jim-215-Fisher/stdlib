4780c43b983f7656970f47656550260864b04ef3
Jim-215-Fisher <53905783+Jim-215-Fisher@users.noreply.github.com>
2021-12-22 16:14:03 +0000

    Deploying to stdlib-fpm from @ Jim-215-Fisher/stdlib@212bc33dd7b1f43a59a8d904c6a17a1e4aae3eab 🚀

--- src/stdlib_stats_distribution_exponential.f90
+++ src/stdlib_stats_distribution_exponential.f90
@@ -7,66 +7,65 @@ module stdlib_stats_distribution_exponential
     implicit none
     private
 
-    real(dp), parameter  :: ONE = 1.0_dp
     integer :: ke(0:255)
     real(dp) :: we(0:255), fe(0:255)
     logical  :: zig_exp_initialized = .false.
 
-    public :: rvs_expon
-    public :: pdf_expon
-    public :: cdf_expon
+    public :: rvs_exp
+    public :: pdf_exp
+    public :: cdf_exp
 
 
 
-    interface rvs_expon
-    !! Version: experimental
+    interface rvs_exp
+    !! Version experimental
     !!
     !! Exponential Distribution Random Variates
     !! ([Specification](../page/specs/stdlib_stats_distribution_exponential.html#
-    !! rvs_expon-exponential-distribution-random-variates))
+    !! rvs_exp-exponential-distribution-random-variates))
     !!
-        module procedure rvs_expon_0_rsp                 !0 dummy variable
+        module procedure rvs_exp_0_rsp                 !0 dummy variable
 
-        module procedure rvs_expon_rsp       !1 dummy variable
-        module procedure rvs_expon_rdp       !1 dummy variable
-        module procedure rvs_expon_csp       !1 dummy variable
-        module procedure rvs_expon_cdp       !1 dummy variable
+        module procedure rvs_exp_rsp       !1 dummy variable
+        module procedure rvs_exp_rdp       !1 dummy variable
+        module procedure rvs_exp_csp       !1 dummy variable
+        module procedure rvs_exp_cdp       !1 dummy variable
 
-        module procedure rvs_expon_array_rsp !2 dummy variables
-        module procedure rvs_expon_array_rdp !2 dummy variables
-        module procedure rvs_expon_array_csp !2 dummy variables
-        module procedure rvs_expon_array_cdp !2 dummy variables
-    end interface rvs_expon
+        module procedure rvs_exp_array_rsp !2 dummy variables
+        module procedure rvs_exp_array_rdp !2 dummy variables
+        module procedure rvs_exp_array_csp !2 dummy variables
+        module procedure rvs_exp_array_cdp !2 dummy variables
+    end interface rvs_exp
 
 
 
-    interface pdf_expon
-    !! Version: experimental
+    interface pdf_exp
+    !! Version experimental
     !!
     !! Exponential Distribution Probability Density Function
     !! ([Specification](../page/specs/stdlib_stats_distribution_exponential.html#
-    !! pdf_expon-exponential-distribution-probability-density-function))
+    !! pdf_exp-exponential-distribution-probability-density-function))
     !!
-        module procedure pdf_expon_rsp
-        module procedure pdf_expon_rdp
-        module procedure pdf_expon_csp
-        module procedure pdf_expon_cdp
-    end interface pdf_expon
+        module procedure pdf_exp_rsp
+        module procedure pdf_exp_rdp
+        module procedure pdf_exp_csp
+        module procedure pdf_exp_cdp
+    end interface pdf_exp
 
 
 
-    interface cdf_expon
-    !! Version: experimental
+    interface cdf_exp
+    !! Version experimental
     !!
     !! Exponential Distribution Cumulative Distribution Function
     !! ([Specification](../page/specs/stdlib_stats_distribution_exponential.html#
-    !! cdf_expon-exponential-distribution-cumulative-distribution-function))
+    !! cdf_exp-exponential-distribution-cumulative-distribution-function))
     !!
-        module procedure cdf_expon_rsp
-        module procedure cdf_expon_rdp
-        module procedure cdf_expon_csp
-        module procedure cdf_expon_cdp
-    end interface cdf_expon
+        module procedure cdf_exp_rsp
+        module procedure cdf_exp_rdp
+        module procedure cdf_exp_csp
+        module procedure cdf_exp_cdp
+    end interface cdf_exp
 
 
 
@@ -78,7 +77,7 @@ contains
     ! Marsaglia & Tsang generator for random normals & random exponentials.
     ! Translated from C by Alan Miller (amiller@bigpond.net.au)
     !
-    ! Marsaglia, G. & Tsang, W.W. (2000) 'The ziggurat method for generating
+    ! Marsaglia, G. & Tsang, W.W. (2000) `The ziggurat method for generating
     ! random variables', J. Statist. Software, v5(8).
     !
     ! This is an electronic journal which can be downloaded from:
@@ -87,6 +86,7 @@ contains
     ! Latest version - 1 January 2001
     !
         real(dp), parameter :: M2 = 2147483648.0_dp, ve = 0.003949659822581572_dp
+        real(dp), parameter :: ONE = 1.0_dp
         real(dp)            :: de, te, q
         integer :: i
 
@@ -113,7 +113,7 @@ contains
 
 
 
-    function rvs_expon_0_rsp( ) result(res)
+    function rvs_exp_0_rsp( ) result(res)
     !
     ! Standard exponential random variate (lambda=1)
     !
@@ -146,9 +146,9 @@ contains
                 end if
            end do L1
        endif
-    end function rvs_expon_0_rsp
+    end function rvs_exp_0_rsp
 
-    function rvs_expon_0_rdp( ) result(res)
+    function rvs_exp_0_rdp( ) result(res)
     !
     ! Standard exponential random variate (lambda=1)
     !
@@ -181,13 +181,13 @@ contains
                 end if
            end do L1
        endif
-    end function rvs_expon_0_rdp
+    end function rvs_exp_0_rdp
 
 
 
 
 
-    function rvs_expon_rsp(lambda) result(res)
+    function rvs_exp_rsp(lambda) result(res)
     !
     ! Exponential distributed random variate
     !
@@ -195,13 +195,13 @@ contains
         real(sp) :: res
 
 
-        if(lambda <= 0.0_sp) call error_stop("Error(rvs_expon): Exponen"   &
+        if(lambda <= 0.0_sp) call error_stop("Error(rvs_exp): Exponen"   &
             //"tial distribution lambda parameter must be greater than zero")
-        res = rvs_expon_0_rsp(  )
+        res = rvs_exp_0_rsp(  )
         res = res / lambda
-    end function rvs_expon_rsp
+    end function rvs_exp_rsp
 
-    function rvs_expon_rdp(lambda) result(res)
+    function rvs_exp_rdp(lambda) result(res)
     !
     ! Exponential distributed random variate
     !
@@ -209,49 +209,49 @@ contains
         real(dp) :: res
 
 
-        if(lambda <= 0.0_dp) call error_stop("Error(rvs_expon): Exponen"   &
+        if(lambda <= 0.0_dp) call error_stop("Error(rvs_exp): Exponen"   &
             //"tial distribution lambda parameter must be greater than zero")
-        res = rvs_expon_0_rdp(  )
+        res = rvs_exp_0_rdp(  )
         res = res / lambda
-    end function rvs_expon_rdp
+    end function rvs_exp_rdp
 
 
 
 
 
-    function rvs_expon_csp(lambda) result(res)
+    function rvs_exp_csp(lambda) result(res)
         complex(sp), intent(in) :: lambda
         complex(sp) :: res
         real(sp) :: tr, ti
 
-        tr = rvs_expon_rsp(lambda % re)
-        ti = rvs_expon_rsp(lambda % im)
+        tr = rvs_exp_rsp(lambda % re)
+        ti = rvs_exp_rsp(lambda % im)
         res = cmplx(tr, ti, kind=sp)
-    end function rvs_expon_csp
+    end function rvs_exp_csp
 
-    function rvs_expon_cdp(lambda) result(res)
+    function rvs_exp_cdp(lambda) result(res)
         complex(dp), intent(in) :: lambda
         complex(dp) :: res
         real(dp) :: tr, ti
 
-        tr = rvs_expon_rdp(lambda % re)
-        ti = rvs_expon_rdp(lambda % im)
+        tr = rvs_exp_rdp(lambda % re)
+        ti = rvs_exp_rdp(lambda % im)
         res = cmplx(tr, ti, kind=dp)
-    end function rvs_expon_cdp
+    end function rvs_exp_cdp
 
 
 
 
 
-    function rvs_expon_array_rsp(lambda, array_size) result(res)
+    function rvs_exp_array_rsp(lambda, array_size) result(res)
         real(sp), intent(in) :: lambda
         integer, intent(in) :: array_size
         real(sp) :: res(array_size), x, re
         real(sp), parameter :: r = 7.69711747013104972_sp
         integer :: jz, iz, i
 
-        if(lambda <= 0.0_sp) call error_stop("Error(rvs_expon_array): Exp" &
-            //"oonential distribution lambda parameter must be greater than zero")
+        if(lambda <= 0.0_sp) call error_stop("Error(rvs_exp_array): Exp" &
+            //"onential distribution lambda parameter must be greater than zero")
 
         if(.not. zig_exp_initialized) call zigset
         do i = 1, array_size
@@ -281,17 +281,17 @@ contains
             endif
             res(i) = re / lambda
         end do
-    end function rvs_expon_array_rsp
+    end function rvs_exp_array_rsp
 
-    function rvs_expon_array_rdp(lambda, array_size) result(res)
+    function rvs_exp_array_rdp(lambda, array_size) result(res)
         real(dp), intent(in) :: lambda
         integer, intent(in) :: array_size
         real(dp) :: res(array_size), x, re
         real(dp), parameter :: r = 7.69711747013104972_dp
         integer :: jz, iz, i
 
-        if(lambda <= 0.0_dp) call error_stop("Error(rvs_expon_array): Exp" &
-            //"oonential distribution lambda parameter must be greater than zero")
+        if(lambda <= 0.0_dp) call error_stop("Error(rvs_exp_array): Exp" &
+            //"onential distribution lambda parameter must be greater than zero")
 
         if(.not. zig_exp_initialized) call zigset
         do i = 1, array_size
@@ -321,13 +321,13 @@ contains
             endif
             res(i) = re / lambda
         end do
-    end function rvs_expon_array_rdp
+    end function rvs_exp_array_rdp
 
 
 
 
 
-    function rvs_expon_array_csp(lambda, array_size) result(res)
+    function rvs_exp_array_csp(lambda, array_size) result(res)
         complex(sp), intent(in) :: lambda
         integer, intent(in) :: array_size
         complex(sp) :: res(array_size)
@@ -335,13 +335,13 @@ contains
         real(sp) :: tr, ti
 
         do i = 1, array_size
-            tr = rvs_expon_rsp(lambda % re)
-            ti = rvs_expon_rsp(lambda % im)
+            tr = rvs_exp_rsp(lambda % re)
+            ti = rvs_exp_rsp(lambda % im)
             res(i) = cmplx(tr, ti, kind=sp)
         end do
-    end function rvs_expon_array_csp
+    end function rvs_exp_array_csp
 
-    function rvs_expon_array_cdp(lambda, array_size) result(res)
+    function rvs_exp_array_cdp(lambda, array_size) result(res)
         complex(dp), intent(in) :: lambda
         integer, intent(in) :: array_size
         complex(dp) :: res(array_size)
@@ -349,115 +349,115 @@ contains
         real(dp) :: tr, ti
 
         do i = 1, array_size
-            tr = rvs_expon_rdp(lambda % re)
-            ti = rvs_expon_rdp(lambda % im)
+            tr = rvs_exp_rdp(lambda % re)
+            ti = rvs_exp_rdp(lambda % im)
             res(i) = cmplx(tr, ti, kind=dp)
         end do
-    end function rvs_expon_array_cdp
+    end function rvs_exp_array_cdp
 
 
 
 
 
-    impure elemental function pdf_expon_rsp(x, lambda) result(res)
+    impure elemental function pdf_exp_rsp(x, lambda) result(res)
     !
     ! Exponential Distribution Probability Density Function
     !
         real(sp), intent(in) :: x, lambda
         real(sp) :: res
 
-        if(lambda <= 0.0_sp) call error_stop("Error(pdf_expon): Expon"     &
+        if(lambda <= 0.0_sp) call error_stop("Error(pdf_exp): Expon"     &
             //"ential distribution lambda parameter must be greater than zero")
-        if(x < 0.0_sp) call error_stop("Error(pdf_expon): Exponential"     &
+        if(x < 0.0_sp) call error_stop("Error(pdf_exp): Exponential"     &
             //" distribution variate x must be non-negative")
         res = exp(- x * lambda) * lambda
-    end function pdf_expon_rsp
+    end function pdf_exp_rsp
 
-    impure elemental function pdf_expon_rdp(x, lambda) result(res)
+    impure elemental function pdf_exp_rdp(x, lambda) result(res)
     !
     ! Exponential Distribution Probability Density Function
     !
         real(dp), intent(in) :: x, lambda
         real(dp) :: res
 
-        if(lambda <= 0.0_dp) call error_stop("Error(pdf_expon): Expon"     &
+        if(lambda <= 0.0_dp) call error_stop("Error(pdf_exp): Expon"     &
             //"ential distribution lambda parameter must be greater than zero")
-        if(x < 0.0_dp) call error_stop("Error(pdf_expon): Exponential"     &
+        if(x < 0.0_dp) call error_stop("Error(pdf_exp): Exponential"     &
             //" distribution variate x must be non-negative")
         res = exp(- x * lambda) * lambda
-    end function pdf_expon_rdp
+    end function pdf_exp_rdp
 
 
 
 
 
-    impure elemental function pdf_expon_csp(x, lambda) result(res)
+    impure elemental function pdf_exp_csp(x, lambda) result(res)
         complex(sp), intent(in) :: x, lambda
         real(sp) :: res
 
-        res = pdf_expon_rsp(x % re, lambda % re)
-        res = res * pdf_expon_rsp(x % im, lambda % im)
-    end function pdf_expon_csp
+        res = pdf_exp_rsp(x % re, lambda % re)
+        res = res * pdf_exp_rsp(x % im, lambda % im)
+    end function pdf_exp_csp
 
-    impure elemental function pdf_expon_cdp(x, lambda) result(res)
+    impure elemental function pdf_exp_cdp(x, lambda) result(res)
         complex(dp), intent(in) :: x, lambda
         real(dp) :: res
 
-        res = pdf_expon_rdp(x % re, lambda % re)
-        res = res * pdf_expon_rdp(x % im, lambda % im)
-    end function pdf_expon_cdp
+        res = pdf_exp_rdp(x % re, lambda % re)
+        res = res * pdf_exp_rdp(x % im, lambda % im)
+    end function pdf_exp_cdp
 
 
 
 
 
-    impure elemental function cdf_expon_rsp(x, lambda) result(res)
+    impure elemental function cdf_exp_rsp(x, lambda) result(res)
     !
     ! Exponential Distribution Cumulative Distribution Function
     !
         real(sp), intent(in) :: x, lambda
         real(sp) :: res
 
-        if(lambda <= 0.0_sp) call error_stop("Error(cdf_expon): Expon"     &
+        if(lambda <= 0.0_sp) call error_stop("Error(cdf_exp): Expon"     &
             //"ential distribution lambda parameter must be greater than zero")
-        if(x < 0.0_sp) call error_stop("Error(cdf_expon): Exponential"     &
+        if(x < 0.0_sp) call error_stop("Error(cdf_exp): Exponential"     &
             //" distribution variate x must be non-negative")
         res = 1.0_sp - exp(- x * lambda)
-    end function cdf_expon_rsp
+    end function cdf_exp_rsp
 
-    impure elemental function cdf_expon_rdp(x, lambda) result(res)
+    impure elemental function cdf_exp_rdp(x, lambda) result(res)
     !
     ! Exponential Distribution Cumulative Distribution Function
     !
         real(dp), intent(in) :: x, lambda
         real(dp) :: res
 
-        if(lambda <= 0.0_dp) call error_stop("Error(cdf_expon): Expon"     &
+        if(lambda <= 0.0_dp) call error_stop("Error(cdf_exp): Expon"     &
             //"ential distribution lambda parameter must be greater than zero")
-        if(x < 0.0_dp) call error_stop("Error(cdf_expon): Exponential"     &
+        if(x < 0.0_dp) call error_stop("Error(cdf_exp): Exponential"     &
             //" distribution variate x must be non-negative")
         res = 1.0_dp - exp(- x * lambda)
-    end function cdf_expon_rdp
+    end function cdf_exp_rdp
 
 
 
 
 
-    impure elemental function cdf_expon_csp(x, lambda) result(res)
+    impure elemental function cdf_exp_csp(x, lambda) result(res)
         complex(sp), intent(in) :: x, lambda
         real(sp) :: res
 
-        res = cdf_expon_rsp(x % re, lambda % re)
-        res = res * cdf_expon_rsp(x % im, lambda % im)
-    end function cdf_expon_csp
+        res = cdf_exp_rsp(x % re, lambda % re)
+        res = res * cdf_exp_rsp(x % im, lambda % im)
+    end function cdf_exp_csp
 
-    impure elemental function cdf_expon_cdp(x, lambda) result(res)
+    impure elemental function cdf_exp_cdp(x, lambda) result(res)
         complex(dp), intent(in) :: x, lambda
         real(dp) :: res
 
-        res = cdf_expon_rdp(x % re, lambda % re)
-        res = res * cdf_expon_rdp(x % im, lambda % im)
-    end function cdf_expon_cdp
+        res = cdf_exp_rdp(x % re, lambda % re)
+        res = res * cdf_exp_rdp(x % im, lambda % im)
+    end function cdf_exp_cdp
 
 
 end module stdlib_stats_distribution_exponential
